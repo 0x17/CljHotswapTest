e98423f4ebdf98bd58957eaedc48f4abdf41904c
0x17 <andreschnabel@me.com>
2013-12-25 16:20:06 +0100

    Initial commit.

---
+++ src/org/andreschnabel/game/main.clj
@@ -0,0 +1,71 @@
+(ns org.andreschnabel.game.main
+  (:import (com.badlogic.gdx Gdx Input Input$Keys Files)
+           (com.badlogic.gdx.graphics.glutils ShapeRenderer ShapeRenderer$ShapeType)
+           (com.badlogic.gdx.graphics GL10 Color Texture)
+           (com.badlogic.gdx.math Vector2)
+           (com.badlogic.gdx.graphics.g2d SpriteBatch)))
+
+(defn init-game []
+  {:sr  (ShapeRenderer.)
+   :pos (Vector2. 10 10)
+   :sb  (SpriteBatch.)
+   :tex (Texture. (.internal Gdx/files "../../../../sprite1.png"))})
+
+(defn quit-game [{:keys [sr sb tex]}]
+  (.dispose sr)
+  (.dispose sb)
+  (.dispose tex))
+
+(defn render-game [{:keys [pos sr sb tex]}]
+  (letfn [(key-pressed? [key]
+                        (.isKeyPressed Gdx/input key))
+
+          (add-pos [dx dy]
+                   (set! (.x pos) (+ (.x pos) dx))
+                   (set! (.y pos) (+ (.y pos) dy)))
+
+          (rect-movement []
+                         (when (key-pressed? Input$Keys/LEFT)
+                           (add-pos -10 0))
+                         (when (key-pressed? Input$Keys/RIGHT)
+                           (add-pos 10 0))
+                         (when (key-pressed? Input$Keys/UP)
+                           (add-pos 0 10))
+                         (when (key-pressed? Input$Keys/DOWN)
+                           (add-pos 0 -10)))
+
+          (process-input []
+                         (when (key-pressed? Input$Keys/ESCAPE)
+                           (.exit Gdx/app))
+                         (when (key-pressed? Input$Keys/W)
+                           (println "Pressed W"))
+                         (rect-movement))
+
+          (clear-scr []
+                     (.glClearColor Gdx/gl 0 0 0 1)
+                     (.glClear Gdx/gl GL10/GL_COLOR_BUFFER_BIT))
+
+          (draw-circles []
+                        (.setColor sr Color/RED)
+                        (doseq [x (range 0 640 100)]
+                          (doseq [y (range 0 480 100)]
+                            (.circle sr x y 100))))
+
+          (draw-square []
+                       (.setColor sr Color/YELLOW)
+                       (.rect sr (.x pos) (.y pos) 100 100))
+
+          (draw-shapes []
+                       (.begin sr ShapeRenderer$ShapeType/Line)
+                       (draw-circles)
+                       (draw-square)
+                       (.end sr))
+          (draw-img []
+                    (.begin sb)
+                    (.draw sb tex (.x pos) (.y pos))
+                    (.end sb))]
+    (process-input)
+    (clear-scr)
+    (draw-shapes)
+    (draw-img)))
+
---
+++ src/org/andreschnabel/hotswap/runner.clj
@@ -0,0 +1,40 @@
+(ns org.andreschnabel.hotswap.runner
+  (:require [org.andreschnabel.game.main :as gmain])
+  (:import (com.badlogic.gdx ApplicationListener Gdx Input Input$Keys)
+           (com.badlogic.gdx.backends.lwjgl LwjglApplication)
+           (java.io File)))
+
+(def code-filename "../game/main.clj")
+
+(defn modified-code? [code-file last-reload]
+  (> (.lastModified code-file) last-reload))
+
+(def my-listener
+  (let [last-reload (atom 0)
+        code-file (File. code-filename)
+        gstate (atom nil)]
+    (proxy [ApplicationListener] []
+      (create []
+        (reset! gstate (gmain/init-game)))
+      (resize [w h])
+      (render []
+        (let [did-modify? (modified-code? code-file @last-reload)]
+          (when did-modify?
+            (try
+              (load-file code-filename)
+              (catch Exception e
+                (println "Reload exception:" (.getMessage e))))
+            (reset! last-reload (.lastModified code-file)))
+          (try
+            (gmain/render-game @gstate)
+            (catch Exception e
+              (when did-modify?
+                (println "Render exception:" (.getMessage e))))))
+        (when (.isKeyPressed Gdx/input Input$Keys/ENTER)
+          (reset! gstate (gmain/init-game))))
+      (pause [])
+      (resume [])
+      (dispose []
+        (gmain/quit-game @gstate)))))
+
+(LwjglApplication. my-listener "Test" 640 480 false)
